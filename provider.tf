a70aaa1baa965fa24b6552b621c6ee10ee66efdd
ambarish2013 <achitnis@gmail.com>
2018-10-11 09:19:28 -0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
+    version         = "~> 1.16.0"
+}
+
+provider "external" {
+    version = "~> 1.0"
+}
